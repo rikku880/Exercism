1b9b696ccdf8edcb3f3ef64e14acc3882481c310
[Riyaz] <riyazpasha.mohammed@thoughtworks.com>
2019-07-22 13:15:43 +0530

    [Palindrome]

--- java/is-palindrome/src/main/java/Palindrome.java
+++ java/is-palindrome/src/main/java/Palindrome.java
@@ -1,26 +1,17 @@
 public class Palindrome {
     public boolean check(String sentence) {
-        sentence = sentence.replaceAll("\\s","");
-        int i = 0, j = sentence.length()-1;
-        sentence = sentence.toLowerCase();
-        int check = 0;
-        while(i<=j){
-            if(sentence.charAt(i) == ':'){
-                i++;
+        sentence=sentence.replaceAll("[^a-zA-Z]","").toLowerCase();
+        int first=0,last=sentence.length()-1;
+        while(first<=last)
+        {
+            if(sentence.charAt(first)==sentence.charAt(last))
+            {
+                first++;
+                last--;
+                continue;
             }
-            else if(sentence.charAt(i) == ':'){
-                j--;
-            }
-            else{
-                if(sentence.charAt(i) != sentence.charAt(j)){
-                    check = 1;
-                    break;
-                }
-            }
-        }
-        if(check==0)
-            return true;
-        else
             return false;
+        }
+        return true;
     }
 }
